0632d68b6c7a80bf1f0cce0742694325a8688ba6
volk1995 <sergijko07@gmail.com>
2025-11-29 08:38:16 +0200

    methodology and before the last update

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,11 @@
 const modal = document.querySelector(".backdrop");
-const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnOpen = document.querySelectorAll(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
 
 const toggleModal = () => modal.classList.toggle("is-hidden");
 
-modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnOpen.forEach((btn) => btn.addEventListener("click", toggleModal));
 modalBtnClose.addEventListener("click", toggleModal);
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) toggleModal();
+});
